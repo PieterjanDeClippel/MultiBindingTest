65517538fb4b9b35cfe1e4ba964f312275449431
Pieterjan De Clippel <pieterjandeclippel@hotmail.com>
2020-05-19 13:22:14 +0200

    Add project files.

---
+++ MultiBindingTest/MultiBindingTest/Converters/BooleanAndConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace MultiBindingTest.Converters
+{
+    public class BooleanAndConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = values.All((val) => (val is bool) && ((bool)val));
+
+            if (targetType == typeof(bool))
+            {
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException($"{nameof(BooleanAndConverter)} can only be used with BindingMode=OneWay");
+        }
+    }
+}
---
+++ MultiBindingTest/MultiBindingTest/ViewModels/AboutViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MultiBindingTest.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        #region Value1
+        private bool value1;
+        public bool Value1
+        {
+            get { return value1; }
+            set
+            {
+                value1 = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #region Value2
+        private bool value2;
+        public bool Value2
+        {
+            get { return value2; }
+            set
+            {
+                value2 = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #region Value3
+        private bool value3;
+        public bool Value3
+        {
+            get { return value3; }
+            set
+            {
+                value3 = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+    }
+}
